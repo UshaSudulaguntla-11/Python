f1598b7c9d4c5036c08fc53500493fe34c527afc
UshaSudulaguntla-11 <ushasudulaguntla11@gmail.com>
2025-08-20 22:18:41 +0530

    Create Grade.py

---
+++ Grade.py
@@ -0,0 +1,13 @@
+n=int(input("Enter Marks:"))
+if n>=90:
+    print("Grade A")
+elif n>=80:
+    print("Grade B")
+elif n>=70:
+    print("Grade C")
+elif n>=60:
+    print("Grade D")
+elif n>=50:
+    print("Grade E")   
+else:
+    print("Fail")
