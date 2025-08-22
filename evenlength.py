bb2ff967c605c5c05d3cb41469561f70f767bada
UshaSudulaguntla-11 <ushasudulaguntla11@gmail.com>
2025-08-22 15:12:46 +0530

    Add files via upload

---
+++ evenlength.py
@@ -0,0 +1,7 @@
+str=str(input("Enter a string:"))
+str=str.split()
+print(str)
+for i in str:
+	if(len(i)%2==0):
+		print(i)
+		
---
+++ len.py
@@ -0,0 +1,5 @@
+str=str(input("Enter a string:"))
+count=0
+for i in str:
+	count+=1
+print("Length of string:",count)
